1ccd06ee1354121aaae4720dc2b494d5ca08bb1d
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2022-05-07 02:12:31 +0200

    Update a2.sh

--- a2.sh
+++ a2.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "a2 started"
-sleep 3
 clear
 pacman -Suy --noconfirm sed
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
@@ -24,20 +22,14 @@ passwd
 pacman --noconfirm -S grub efibootmgr
 echo "Enter EFI partition: " 
 read efipartition
-sleep 3
 mkdir /boot/EFI
-sleep 3
 mount $efipartition /boot/EFI 
-sleep 3
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
-sleep 3
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 3
 
 pacman -S --noconfirm networkmanager neovim man-db wget git doas \
         ttf-dejavu zip unzip unrar p7zip gzip bzip2 feh virtualbox-guest-utils-nox zsh
-sleep 3
 
 systemctl enable NetworkManager.service
 
@@ -47,6 +39,4 @@ useradd -m -G wheel,audio,video,optical,storage -s /bin/zsh $username
 passwd $username
 echo "permit $username as root" > /etc/doas.conf
 
-echo "a2 finished, will exit"
-sleep 3
 exit
