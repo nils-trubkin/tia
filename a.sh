eb83d3a54d4297bafd9d383e9e556fe72664b18e
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2022-05-07 02:22:02 +0200

    Update a.sh

--- a.sh
+++ a.sh
@@ -43,14 +43,8 @@ pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 systemctl daemon-reload
 
-echo "Will download a2"
-sleep 3
-
 curl https://raw.githubusercontent.com/nils-trubkin/rmd/master/a2.sh > /mnt/a2.sh
 chmod +x /mnt/a2.sh
-
-echo "Will run a2"
-sleep 3
 
 arch-chroot /mnt ./a2.sh
 
