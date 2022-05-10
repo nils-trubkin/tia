a40a34c92411676f0f6a2060eb870b9358559d3c
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2022-05-10 07:05:10 +0200

    Update a3.sh

--- a3.sh
+++ a3.sh
@@ -15,9 +15,9 @@ sudo aura -A --noconfirm zsh-theme-powerlevel10k-git zsh-vi-mode brave-beta-bin
 read -p $'\nConnect NAS? [y/N] ' nas_ans
 if [[ $nas_ans = y ]] ; then
   read -p $'\nActivate VM clipboard? [y/N] ' vm_ans
-    if [[ $vm_ans = y ]] ; then
-      VBoxClient --clipboard
-    fi
+  if [[ $vm_ans = y ]] ; then
+     VBoxClient --clipboard
+  fi
   read -p 'hostname: ' nas_host
   read -p 'user: ' nas_usr
   read -sp 'password: ' nas_psd
@@ -32,14 +32,14 @@ if [[ $nas_ans = y ]] ; then
   doas mount -t cifs -o username=$nas_user,password=$nas_pass,uid=$nas_uid //$nas_host/$nas_vol $nas_mnt
   
   read -p $'\nDownload SSH keys? [y/N] ' ssh_ans
-    if [[ $ssh_ans = y ]] ; then
-      cp $nas_mnt/ssh/id_ed25519* ~/.ssh/
-      read -p $'\nDownload dot? [y/N] ' dot_ans
-      if [[ $dot_ans = y ]] ; then
-        echo 'git clone dot && run a4.sh'
-        #git clone git@github.com:nils-trubkin/dot.git
-      fi
+  if [[ $ssh_ans = y ]] ; then
+    cp $nas_mnt/ssh/id_ed25519* ~/.ssh/
+    read -p $'\nDownload dot? [y/N] ' dot_ans
+    if [[ $dot_ans = y ]] ; then
+      echo 'git clone dot && run a4.sh'
+      #git clone git@github.com:nils-trubkin/dot.git
     fi
+  fi
 fi
 
 exit
