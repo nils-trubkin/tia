c47cc5a48bdebb013ea6019fc7c0c8e92571b43e
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2022-05-10 22:11:08 +0200

    Update a3.sh

--- a3.sh
+++ a3.sh
@@ -20,16 +20,17 @@ if [[ $nas_ans = y ]] ; then
   echo ''
   id
   read -p 'uid: ' nas_uid
-  read -p 'volume on NAS: ' nas_vol
-  read -p 'local mount point: ' nas_mnt
+  read -p 'volume on NAS (do not use quotes): ' nas_vol
+  read -p 'local mount point (absolute path): ' nas_mnt
   
   # Create mount point and mount NAS
   mkdir -p $nas_mnt
   doas pacman -Suy --noconfirm cifs-utils
-  doas mount -t cifs -o username=$nas_usr,password=$nas_psd,uid=$nas_uid //$nas_host/$nas_vol $nas_mnt
+  doas mount -t cifs -o username="$nas_usr",password="$nas_psd",uid="$nas_uid" //"$nas_host"/"$nas_vol" "$nas_mnt"
   
   read -p $'\nDownload SSH keys? [y/N] ' ssh_ans
   if [[ $ssh_ans = y ]] ; then
+    mkdir ~/.ssh
     cp $nas_mnt/ssh/id_ed25519* ~/.ssh/
     read -p $'\nDownload dot? [y/N] ' dot_ans
     if [[ $dot_ans = y ]] ; then
