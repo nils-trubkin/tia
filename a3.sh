a07e47039840273d1b36a7ab6104f960bca341cf
Nils Trubkin <32000568+nils-trubkin@users.noreply.github.com>
2022-05-10 05:30:58 +0200

    Update a3.sh

--- a3.sh
+++ a3.sh
@@ -12,33 +12,32 @@ doas pacman --noconfirm -U aura-bin-*
 # Install zsh 10k
 sudo aura -A --noconfirm zsh-theme-powerlevel10k-git zsh-vi-mode brave-beta-bin
 
-read -p "Connect NAS? [y/N]" nas_ans
+read -p $'\nConnect NAS? [y/N] ' nas_ans
 if [[ $nas_ans = y ]] ; then
-  echo "hostname: "
-  read nas_host
-  echo "user: "
-  read nas_usr
-  echo "password: "
-  read nas_psd
+  read -p $'\nActivate VM clipboard? [y/N] ' vm_ans
+    if [[ $vm_ans = y ]] ; then
+      VBoxClient --clipboard
+    fi
+  read -p 'hostname: ' nas_host
+  read -p 'user: ' nas_usr
+  read -sp 'password: ' nas_psd
   id
-  echo "uid: "
-  read nas_uid
-  echo "volume on NAS: "
-  read nas_vol
-  echo "local mount point: "
-  read nas_mnt
+  read -p $'\nuid: ' nas_uid
+  read -p 'volume on NAS: ' nas_vol
+  read -p 'local mount point: ' nas_mnt
   
   # Create mount point and mount NAS
   mkdir -p $nas_mnt
   doas pacman -Suy --noconfirm cifs-utils
-  doas mount -t cifs -o username=$nas_user,password=$nas_pass,uid=$nas_uid //$hostname/$nas_vol $nas_mnt
+  doas mount -t cifs -o username=$nas_user,password=$nas_pass,uid=$nas_uid //$nas_host/$nas_vol $nas_mnt
   
-  read -p "Download SSH keys? [y/N]" ssh_ans
+  read -p $'\nDownload SSH keys? [y/N] ' ssh_ans
     if [[ $ssh_ans = y ]] ; then
       cp $nas_mnt/ssh/id_ed25519* ~/.ssh/
-      read -p "Download dot? [y/N]" rmdp_ans
-      if [[ $rmdp_ans = y ]] ; then
-        git clone git@github.com:nils-trubkin/dot.git
+      read -p $'\nDownload dot? [y/N] ' dot_ans
+      if [[ $dot_ans = y ]] ; then
+        echo 'git clone dot && run a4.sh'
+        #git clone git@github.com:nils-trubkin/dot.git
     fi
 fi
 
